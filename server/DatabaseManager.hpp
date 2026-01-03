7f7ccdb5997d648c78cb2d306212c04a8d460c9b
Isari Andrew <andrewisari5@gmail.com>
2026-01-03 20:09:19 +0200

    Update DatabaseManager.hpp

---
+++ server/DatabaseManager.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <optional>
+#include <mutex>
+
+#include <sqlite3.h>
+
+namespace net_ops::server {
+    struct UserRecord {
+        int id;
+        std::string username;
+        std::vector<uint8_t> password_hash;
+        std::vector<uint8_t> salt;
+    };
+
+    class DatabaseManager {
+        private:
+            sqlite3* db_;
+            std::mutex db_mutex_;
+            sqlite3_stmt* stmt_insert_user_;
+            sqlite3_stmt* stmt_get_user_;
+
+            DatabaseManager();
+        public:
+            static DatabaseManager& GetInstance();
+
+            DatabaseManager(const DatabaseManager&) = delete;
+            DatabaseManager& operator=(const DatabaseManager&) = delete;
+
+            bool Initialize(const std::string& db_path);
+            void Shutdown();
+
+            bool CreateUser(std::string username, std::vector<uint8_t> password_hash, std::vector<uint8_t> salt);
+            std::optional<UserRecord> GetUserByName(std::string username);
+    };
+}
